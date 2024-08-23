fdd231c84c55dc186e65e450e867ace934847457
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-08-23 15:28:39 -0600

    Implement candidate filtering methods

--- filters.rb
+++ filters.rb
@@ -1,16 +1,28 @@
-# In this file we define the methods to help filter out candidates
-# This way, we keep these methods separated from other potential parts of the program
+# In this file, we define the methods to help filter out candidates.
+# This way, we keep these methods separated from other potential parts of the program.
 
 def find(id)
-    # Your code Here
+  @candidates.find { |candidate| candidate[:id] == id }
+end
+
+def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
+end
+
+def applied_recently?(candidate)
+  (Date.today - candidate[:date_applied]).to_i <= 15
+end
+
+def qualified_candidates(candidates)
+  candidates.select do |candidate|
+    experienced?(candidate) &&
+    candidate[:github_points] >= 100 &&
+    (candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')) &&
+    applied_recently?(candidate) &&
+    candidate[:age] > 17
   end
-  
-  def experienced?(candidate)
-    # Your code Here
-  end
-  
-  def qualified_candidates(candidates)
-    # Your code Here
-  end
-  
-  # More methods will go below
+end
+
+def ordered_by_qualifications(candidates)
+  candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+end
